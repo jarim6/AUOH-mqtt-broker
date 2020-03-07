d5572d52a5dc6bbd66deab36b5674a43153b9d94
Jari Mutanen <jari.mutanen@genexis.eu>
2020-03-07 13:29:53 +0200

    update html connection options rename link

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
 //const port = 8888
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 443;
 
 ws.createServer({ server: httpServer }, aedes.handle)
 
